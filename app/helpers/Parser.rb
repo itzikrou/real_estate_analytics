c7feac7733b3ed9f882b09a4a283d2d4c2d441cf
Itay Mishan <mishan.itay@gmail.com>
2016-06-10 00:28:33 -0400

    parser

--- app/helpers/Parser.rb
+++ app/helpers/Parser.rb
@@ -1,24 +1,36 @@
-require 'wombat'
+require 'open-uri'
 
-class parser
+class Parser
 
-Wombat.crawl do
-  base_url "http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=8f41647c2786457da2c575a64a4bb785&App=TREB"
-  path "/"
+	def self.parse
+		Wombat.crawl do
+		  base_url "http://v3.torontomls.net"
+		  path "/Live/Pages/Public/Link.aspx?Key=0c00bf31977a4a76a78484c9a6cbed52&App=TREB"
 
-  headline xpath: "//h1"
-  subheading css: "p.subheading"
+		  links do
+		    explore xpath: '//*[@class="data-list"]' do |e|
+debugger
+		      e.gsub(/  /, " ") 
+		    end
+		  end
+		end
+	end
 
-  what_is({ css: ".one-half h3" }, :list)
 
-  links do
-    explore xpath: '//*[@class="wrapper"]/div[1]/div[1]/div[2]/ul/li[1]/a' do |e|
-      e.gsub(/Explore/, "Love")
-    end
+	def self.noko
+		# doc = Nokogiri::XML(File.open("http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=8f41647c2786457da2c575a64a4bb785&App=TREB")) do |config|
+		#   config.strict.nonet
+		doc = Nokogiri::HTML(open("http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=0c00bf31977a4a76a78484c9a6cbed52&App=TREB")) do |config|
+  			config.strict.nonet.noblanks
+		end
+	end
 
-    features css: '.features'
-    enterprise css: '.enterprise'
-    blog css: '.blog'
-  end
-end
+		def self.noko1
+			html = ScraperWiki::scrape("http://v3.torontomls.net/Live/Pages/Public/Link.aspx?Key=0c00bf31977a4a76a78484c9a6cbed52&App=TREB")
+debugger			
+			doc = Nokogiri::HTML(html)
+			doc.css('table.data-list-row-number tr').each do |link| puts link.to_html end
+			item = doc.css('table.data-list tr').each do |link| puts link['data-pop-up'] end
+		end
+
 end
