ed5c49703d491281498c560c9f09d90911a99f81
Itay Mishan <itaymishan@users.noreply.github.com>
2016-06-28 19:09:20 -0400

    Update listing_data_items.rb
    comment out stuff

--- app/services/listing_data_items.rb
+++ app/services/listing_data_items.rb
@@ -14,41 +14,41 @@ module ListingDataItems
 		]
 	end
 
-	self.sold = [
-		"List:","Leased:", "Sold:", "For:", "Taxes:", "% Dif:", "Sold Date:", 
-		"Contract Date:", "Leased Date:", 
-		"SPIS:", "Last Status:", "Rms:", "Bedrooms:", "DOM:", "Lot:",
-		"Washrooms:", "Dir/Cross St:", "MLS#:", "Lease Term:", "Kitchens:",
-		"Basement:", "Heat:", "Apx Age:", "Apx Sqft:", "Exposure:", "Lndry Acc:",
-		"Maintanance:", "A/C:", "All Incl:", "Water Incl:", "Heat Incl:",
-		"Hydro Incl:", "Cable TV Incl:", "Bldg Ins Incl:", "Prkg Incl:", "Furnished:",
-		"Garage:", "Park/Drive:", "Park Type:", "Park Spcs:", "Bldg Amen:", 
-		"Client Remks:", "Extras:"
-	]
+	# self.sold = [
+	# 	"List:","Leased:", "Sold:", "For:", "Taxes:", "% Dif:", "Sold Date:", 
+	# 	"Contract Date:", "Leased Date:", 
+	# 	"SPIS:", "Last Status:", "Rms:", "Bedrooms:", "DOM:", "Lot:",
+	# 	"Washrooms:", "Dir/Cross St:", "MLS#:", "Lease Term:", "Kitchens:",
+	# 	"Basement:", "Heat:", "Apx Age:", "Apx Sqft:", "Exposure:", "Lndry Acc:",
+	# 	"Maintanance:", "A/C:", "All Incl:", "Water Incl:", "Heat Incl:",
+	# 	"Hydro Incl:", "Cable TV Incl:", "Bldg Ins Incl:", "Prkg Incl:", "Furnished:",
+	# 	"Garage:", "Park/Drive:", "Park Type:", "Park Spcs:", "Bldg Amen:", 
+	# 	"Client Remks:", "Extras:"
+	# ]
 
-	self.for_lease = [
-		"List:","Leased:", "Sold:", "For:", "Taxes:", "% Dif:", "Sold Date:", 
-		"Contract Date:", "Leased Date:", 
-		"SPIS:", "Last Status:", "Rms:", "Bedrooms:", "DOM:", "Lot:",
-		"Washrooms:", "Dir/Cross St:", "MLS#:", "Lease Term:", "Kitchens:",
-		"Basement:", "Heat:", "Apx Age:", "Apx Sqft:", "Exposure:", "Lndry Acc:",
-		"Maintanance:", "A/C:", "All Incl:", "Water Incl:", "Heat Incl:",
-		"Hydro Incl:", "Cable TV Incl:", "Bldg Ins Incl:", "Prkg Incl:", "Furnished:",
-		"Garage:", "Park/Drive:", "Park Type:", "Park Spcs:", "Bldg Amen:", 
-		"Client Remks:", "Extras:"
-	]
+	# self.for_lease = [
+	# 	"List:","Leased:", "Sold:", "For:", "Taxes:", "% Dif:", "Sold Date:", 
+	# 	"Contract Date:", "Leased Date:", 
+	# 	"SPIS:", "Last Status:", "Rms:", "Bedrooms:", "DOM:", "Lot:",
+	# 	"Washrooms:", "Dir/Cross St:", "MLS#:", "Lease Term:", "Kitchens:",
+	# 	"Basement:", "Heat:", "Apx Age:", "Apx Sqft:", "Exposure:", "Lndry Acc:",
+	# 	"Maintanance:", "A/C:", "All Incl:", "Water Incl:", "Heat Incl:",
+	# 	"Hydro Incl:", "Cable TV Incl:", "Bldg Ins Incl:", "Prkg Incl:", "Furnished:",
+	# 	"Garage:", "Park/Drive:", "Park Type:", "Park Spcs:", "Bldg Amen:", 
+	# 	"Client Remks:", "Extras:"
+	# ]
 
-	self.leased = [
-		"List:","Leased:", "Sold:", "For:", "Taxes:", "% Dif:", "Sold Date:", 
-		"Contract Date:", "Leased Date:", 
-		"SPIS:", "Last Status:", "Rms:", "Bedrooms:", "DOM:", "Lot:",
-		"Washrooms:", "Dir/Cross St:", "MLS#:", "Lease Term:", "Kitchens:",
-		"Basement:", "Heat:", "Apx Age:", "Apx Sqft:", "Exposure:", "Lndry Acc:",
-		"Maintanance:", "A/C:", "All Incl:", "Water Incl:", "Heat Incl:",
-		"Hydro Incl:", "Cable TV Incl:", "Bldg Ins Incl:", "Prkg Incl:", "Furnished:",
-		"Garage:", "Park/Drive:", "Park Type:", "Park Spcs:", "Bldg Amen:", 
-		"Client Remks:", "Extras:"
-	]
+	# self.leased = [
+	# 	"List:","Leased:", "Sold:", "For:", "Taxes:", "% Dif:", "Sold Date:", 
+	# 	"Contract Date:", "Leased Date:", 
+	# 	"SPIS:", "Last Status:", "Rms:", "Bedrooms:", "DOM:", "Lot:",
+	# 	"Washrooms:", "Dir/Cross St:", "MLS#:", "Lease Term:", "Kitchens:",
+	# 	"Basement:", "Heat:", "Apx Age:", "Apx Sqft:", "Exposure:", "Lndry Acc:",
+	# 	"Maintanance:", "A/C:", "All Incl:", "Water Incl:", "Heat Incl:",
+	# 	"Hydro Incl:", "Cable TV Incl:", "Bldg Ins Incl:", "Prkg Incl:", "Furnished:",
+	# 	"Garage:", "Park/Drive:", "Park Type:", "Park Spcs:", "Bldg Amen:", 
+	# 	"Client Remks:", "Extras:"
+	# ]
 
 
 end
