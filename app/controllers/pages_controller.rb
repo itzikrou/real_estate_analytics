4b27c108c20c34d0cc7c7f8f346e91c6a1f57525
Itay Mishan <itaymishan@users.noreply.github.com>
2016-07-07 11:28:39 -0400

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,14 @@
 class PagesController < ApplicationController
-  
+
   skip_before_filter :verify_authenticity_token
 
   def index
-    @pages = Page.all
+    @pages = Page.where.not(url: nil)
   end
 
   def create
     if params[:url].present?
-      @page = Page.create!(url: params[:url])
+      @page = Page.find_or_create_by(url: params[:url])
     end
   end
 end
