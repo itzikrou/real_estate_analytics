fd37a30abba7d685073f91f9d8e3e46b5058c8db
Itay Mishan <mishan.itay@gmail.com>
2016-08-09 08:26:22 -0400

    add some stuff

--- app/models/sale_listing.rb
+++ app/models/sale_listing.rb
@@ -72,12 +72,12 @@ class SaleListing < ActiveRecord::Base
   before_save :calculate_expected_return_rate
 
 # results = Geocoder.search("#328 - 60 FAIRFAX CRES,Toronto, Ontario M1L1Z8")
+# Client.where("first_name LIKE '%#{params[:first_name]}%'")
 
   def calculate_expected_return_rate
     rented_avarage = RentListing.near(coordinate, 1, units: :km)
                       .where(bedrooms: self.bedrooms)
                       .average(:asking_price)
-
     if rented_avarage.present?
       self.expected_return_rate = ((rented_avarage * 12) / asking_price) * 100
     end
--- commands.rb
+++ commands.rb
@@ -3,7 +3,7 @@ RealtorEntry.all.each{|re| RealtorDataAdapter.new(re.data).create_data_entry}
 
 RealtorEntry.all.each{|re| RealtorDataAdapter.new(re.data).create_data_entry}
 
- i = 0; while i<100 do Property.all.sample.fetch_current_nearby ; i = i+1 ; sleep(2) end
+ i = 0; while i<20 do Property.all.sample.fetch_current_nearby ; i = i+1 ; sleep(20) end
 
   i = 0; while i<200 do Property.all.sample.fetch_current_nearby ; i = i+1 ; sleep(2) end
 
