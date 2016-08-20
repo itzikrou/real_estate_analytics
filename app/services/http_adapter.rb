07d608f1aa6577b0386e99defd264e5262d42ef1
Itay Mishan <mishan.itay@gmail.com>
2016-08-19 21:37:40 -0400

    change adapter

--- app/services/http_adapter.rb
+++ app/services/http_adapter.rb
@@ -15,5 +15,4 @@ class HttpAdapter
   def self.body(longitude_min, longitude_max, latitude_min, latitude_max)
     "CultureId=1&ApplicationId=1&RecordsPerPage=9&MaximumResults=9&PropertySearchTypeId=1&TransactionTypeId=1&PropertyTypeGroupID=1&LongitudeMin=#{longitude_min}&LongitudeMax=#{longitude_max}&LatitudeMin=#{latitude_min}&LatitudeMax=#{latitude_max}"
   end
-
 end
