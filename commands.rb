2e57ad4a6601e020b6bc3f7566f7a2538c62d820
Itay Mishan <itaymishan@users.noreply.github.com>
2016-09-04 10:30:01 -0400

    Update commands.rb

--- commands.rb
+++ commands.rb
@@ -6,5 +6,6 @@ i = 0; while i<200 do Property.all.sample.fetch_current_nearby ; i = i+1 ; sleep
 Property.not_exported.each{|p| res = MlsEmailDataAdapter.new(p.parsed_data).create_data_entry; p.exported_at = DateTime.now if res; p.save!}
 RealtorEntry.not_exported.each{|re| res = RealtorDataAdapter.new(re.data).create_data_entry; re.exported_at = DateTime.now if res; re.save!}
 EmailWorker.perform
+ExportWorker.perform
 i = 0; while i<20 do SaleListing.all.sample.fetch_current_nearby ; i = i+1 ; sleep(20) end
 i = 0; while i<20 do RentListing.all.sample.fetch_current_nearby ; i = i+1 ; sleep(20) end
